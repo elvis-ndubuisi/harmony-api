efd67fd6f08c505ef0f71151d979d522e658c9a7
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-08 08:37:37 +0100

    Refactored

--- controllers/uptown.controllers.js
+++ controllers/uptown.controllers.js
@@ -1,7 +1,13 @@
+/** Send contact and message to updownstudio
+ * Uptownstudio receives email message
+ */
 export function contactUptown(req, res) {
   res.send("Contact me");
 }
 
+/** Book a session from uptownstudio website
+ * Uptownstudio receives email message
+ */
 export function bookSession() {
   res.send("Book session");
 }
--- routes.js
+++ routes.js
@@ -6,18 +6,12 @@ import { contactUptown, bookSession } from "./controllers/uptown.controllers";
 
 const router = express.Router();
 
-/** Send contact and message to updownstudio
- * Uptownstudio receives email message
- */
 router.post(
   "/upt/contact",
   cors({ origin: config.get("cors.uptownstudio") }),
   contactUptown
 );
 
-/** Book a session from uptownstudio website
- * Uptownstudio receives email message
- */
 router.post(
   "/upt/session",
   cors({ origin: config.get("cors.uptownstudio") }),
